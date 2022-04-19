d32e00dfc07b449638b4d0c1ae121041b5379c86
anish jain <helloanishjain@gmail.com>
2022-04-19 09:57:32 +0530

    why we brought this domain

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="">
-      <div className="text-red-800 text-xl text-center">Labs - We own this Domain</div>
-      <div className="max-w-lg mx-auto flex justify-between items-center mt-8 ">
+      <div className="text-red-800 text-xl text-center mt-4">Labs - We own this Domain</div>
+      <div className="max-w-lg mx-auto flex justify-between items-center m-16 ">
 
         <a href="https://twitter.com/itsanishjain">
           <p className="text-blue-500 underline">Anish Jain</p>
@@ -13,6 +13,9 @@ export default function Home() {
           <p className="text-blue-500 underline">Sahil Aujla</p>
           <p>Full Stack Blockchain Developer</p>
         </a>
+      </div>
+      <div className="bg-gray-200 max-w-lg mx-auto rounded-md p-4">
+        We stumbled upon a Yuga Labs Twitter account which had 20k followers and thought that it was the official account but when we visited the site that they mentioned in their description it was up for sale, we thought it would be a good chance to grab this domain and sell for profit in the future. We bought the domain and then we found that this was not the official Yuga Labs account.
       </div>
     </div>
   );
