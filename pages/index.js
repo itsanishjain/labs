7f8426bbf996d3e1718ea6911bb4f19f359ecb83
anish jain <helloanishjain@gmail.com>
2022-04-19 09:24:59 +0530

    add twitter links

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
     <div className="">
-      <div className="text-red-800 text-xl text-center">Labs</div>
-      <div className="max-w-lg mx-auto">
-        <p>Anish Jain</p>
-        <p>Full Stack Blockchain Developer</p>
+      <div className="text-red-800 text-xl text-center">Labs - We own this Domain</div>
+      <div className="max-w-lg mx-auto flex justify-between items-center mt-8 ">
+
+        <a href="https://twitter.com/itsanishjain">
+          <p className="text-blue-500 underline">Anish Jain</p>
+          <p>Full Stack Blockchain Developer</p>
+        </a>
+
+        <a href="https://twitter.com/SahilAujla15">
+          <p className="text-blue-500 underline">Sahil Aujla</p>
+          <p>Full Stack Blockchain Developer</p>
+        </a>
       </div>
     </div>
   );
