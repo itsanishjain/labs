0332dde05503a2bcc11713fb50b2709baf462102
anish jain <helloanishjain@gmail.com>
2022-04-19 09:05:21 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="">
+      <div className="text-red-800 text-xl text-center">Labs</div>
+      <div className="max-w-lg mx-auto">
+        <p>Anish Jain</p>
+        <p>Full Stack Blockchain Developer</p>
+      </div>
+    </div>
+  );
+}
